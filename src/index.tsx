dd9a385e6a83c8c61a3ca667a2943f6ce83aef65
Al <al.almaktoum@live.com>
2020-10-25 10:25:58 +1100

    Setup Backend and basic Authentication

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import Amplify from 'aws-amplify'
+import awsmobile from './aws-exports'
+//GraphQL endpoint
+//https://xbhf5e2uwfg7ppqogampjlwlua.appsync-api.ap-southeast-1.amazonaws.com/graphql
+Amplify.configure(awsmobile)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
